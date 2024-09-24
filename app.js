fc351a669025e6a3f4822a82c5222e35912c6c46
priyosu <suxpriyo@gmail.com>
2024-09-25 03:22:32 +0600

    made it responsive

---
+++ app.js
@@ -0,0 +1,10 @@
+let cl = document.querySelector(".slide");
+let sli = document.querySelector(".menu");
+console.log(cl);
+
+function sidebar() {
+  sli.style.display = "block";
+}
+function displayHide() {
+  sli.style.display = "none";
+}
